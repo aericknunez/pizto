70a9e59e4d2dc63311f2100ee38152efcc2516c6
Erick Nunez <aerick.nunez@gmail.com>
2019-05-14 14:59:15 -0300

    Update import_hash.php

--- sync/import_hash.php
+++ sync/import_hash.php
@@ -6,47 +6,57 @@ define("USER", "superpol_erick"); 			// The database username.
 define("PASSWORD", "caca007125-"); 	// The database password. 
 define("DATABASE", "superpol_pizto");  
 
-
 require_once("/home/superpol/public_html/pizto.com/admin/application/common/Mysqli.php");
 $db = new dbConn();
 
+
+
 // busca todos los archivos en el directorio
 $archivos = glob("/home/superpol/public_html/pizto.com/admin/sync/db/*.sql");  
-  foreach($archivos as $data){  
+  foreach($archivos as $data){ 
+
   	$data = str_replace("/home/superpol/public_html/pizto.com/admin/sync/db/", "", $data);
   	$hash = str_replace(".sql", "", $data);
 
     $archx = "/home/superpol/public_html/pizto.com/admin/sync/db/" . $data;            
 
-    // compruebo si ya hay registro tipo 2 del sync
-$a = $db->query("SELECT * FROM login_sync WHERE hash = '$hash' and tipo = 1");
-if($a->num_rows == 0){
-        
-        if (file_exists($archx)) {
-	    $sql = explode(";",file_get_contents($archx));//
+// obtener el td y type del archivo
+    $fecha = date("d-m-Y");
+  	$numero = strpos($hash, "-");
+	$td = substr($hash,0,$numero);
+	$type = substr($hash,$numero + 1,$numero);
+
+
+
+// primero compruebo si es 1 o 5, respaldo o sincronizcion
+	if($type == 5){
+		// compruebo si ya hay backup tipo 1 del sync del dia y td
+		$a = $db->query("SELECT * FROM login_sync WHERE fecha = '$fecha' and tipo = 1 and td = '$td'");
+		if($a->num_rows == 0){ // si no hay un respaldo
+			if (file_exists($archx)) {
+		    $sql = explode(";",file_get_contents($archx));//
 			foreach($sql as $query){
 			@$db->query($query);
 			} @unlink($archx); } 
 
+		} else { // si hay respaldo
+		    @unlink($archx);	
+		}
+
+
 	} else {
-		// obtener el td segun el nombre del archivo
-		  	$numero = strpos($hash, "-");
-			$td = substr($hash,0,$numero);
-			$type = substr($hash,$numero + 1,$numero);
 
-			    if ($r = $db->select("td", "login_sync", "WHERE hash = '$hash'")) { 
-			        $tdx = $r["td"];
-			    } unset($r);
-			    if($td == $tdx and $type == 5){
-			    	@unlink($archx);	
-			    }  
+		// si no es sincronizacion lo ejecuto siempre
+			if (file_exists($archx)) {
+		    $sql = explode(";",file_get_contents($archx));//
+			foreach($sql as $query){
+			@$db->query($query);
+			} @unlink($archx); } 
 
-	} $a->close();  
 
-}
+	} // termina comprobacion si es sincronizacion o backup
 
 
 
 
-	                  
-	            
+} // termina busqueda de archivos en la carpeta
