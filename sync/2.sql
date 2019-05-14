9a8bcf747ad950007119b2cb936bc2886b2989d1
Erick Nunez <aerick.nunez@gmail.com>
2019-05-14 05:14:38 -0300

    Sin Sql

--- sync/2.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `sync_up` (`id`, `tabla`, `edo`, `td`) VALUES
-('', 'alter_opciones', 0, 2),
-('', 'alter_producto_reporte', 0, 2),
-('', 'facturar_cai', 0, 2),
-('', 'facturar_impresora', 0, 2),
-('', 'facturar_rtn', 0, 2),
-('', 'facturar_rtn_cliente', 0, 2),
-('', 'facturar_ticket', 0, 2),
-('', 'facturar_users', 0, 2);
--- sync/5.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `sync_up` (`id`, `tabla`, `edo`, `td`) VALUES
-('', 'alter_opciones', 0, 5),
-('', 'alter_producto_reporte', 0, 5),
-('', 'facturar_cai', 0, 5),
-('', 'facturar_impresora', 0, 5),
-('', 'facturar_rtn', 0, 5),
-('', 'facturar_rtn_cliente', 0, 5),
-('', 'facturar_ticket', 0, 5),
-('', 'facturar_users', 0, 5);
