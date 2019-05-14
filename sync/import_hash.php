d3eecbffc473b6b94d3ce66cc731c5be498fd908
Erick Nunez <aerick.nunez@gmail.com>
2019-05-14 08:00:51 -0300

    Variables de Remote

--- sync/import_hash.php
+++ sync/import_hash.php
@@ -1,5 +1,12 @@
 <?php
-require_once("/home/superpol/public_html/pizto.com/admin/application/includes/variables_db.php");
+date_default_timezone_set('America/El_Salvador');
+
+define("HOST", "localhost"); 			//35.225.56.157 The host you want to connect to. 
+define("USER", "superpol_erick"); 			// The database username. 
+define("PASSWORD", "caca007125-"); 	// The database password. 
+define("DATABASE", "superpol_pizto");  
+
+
 require_once("/home/superpol/public_html/pizto.com/admin/application/common/Mysqli.php");
 $db = new dbConn();
 
