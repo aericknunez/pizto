9d9237f1e17c82163dd5e629d2d9392c0129d1da
Erick Nunez <aerick.nunez@gmail.com>
2019-05-17 02:39:52 -0600

    Update actualizaciones.php

--- system/sync/actualizaciones.php
+++ system/sync/actualizaciones.php
@@ -5,5 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <h1>ACTUALIZACIONES</h1>
 
 
+<div class="row justify-content-md-center" id="loaderx">
+    <img src="assets/img/loading.gif" alt=""></div>
 
 <div id="mostrar"></div>
