07b8714a3c7d5333fc4f3cf6cf82bdb5008a4db0
Erick Nunez <aerick.nunez@gmail.com>
2019-05-20 22:38:29 -0600

    Arreglos  en Logout

--- application/includes/variables_db.php
+++ application/includes/variables_db.php
@@ -1,19 +1,19 @@
 <?php
 date_default_timezone_set('America/El_Salvador');
 
-if($_SERVER["SERVER_NAME"] != "pizto.com" or $_SERVER["SERVER_NAME"] != "superpollo.net"){
-
-define("HOST", "localhost"); 			//35.225.56.157 The host you want to connect to. 
-define("USER", "root"); 			// The database username. 
-define("PASSWORD", "erick"); 	// The database password. 
-define("DATABASE", "pizto_ventas"); 
-
-} else {
+if($_SERVER["SERVER_NAME"] == "pizto.com" or $_SERVER["SERVER_NAME"] == "superpollo.net"){
 
 define("HOST", "localhost"); 			//35.225.56.157 The host you want to connect to. 
 define("USER", "superpol_erick"); 			// The database username. 
 define("PASSWORD", "caca007125-"); 	// The database password. 
 define("DATABASE", "superpol_pizto");  
+
+} else {
+
+define("HOST", "localhost"); 			//35.225.56.157 The host you want to connect to. 
+define("USER", "root"); 			// The database username. 
+define("PASSWORD", "erick"); 	// The database password. 
+define("DATABASE", "pizto_ventas"); 
 
 }
 
