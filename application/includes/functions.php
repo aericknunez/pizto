27f1653f5deb5fab9af53364beb1a32a00e0a695
Erick Nunez <aerick.nunez@gmail.com>
2019-05-20 22:27:20 -0600

    Update functions.php

--- application/includes/functions.php
+++ application/includes/functions.php
@@ -8,7 +8,7 @@ function sec_session_start() {
 
     // Forces sessions to only use cookies.
     if (ini_set('session.use_only_cookies', 1) === FALSE) {
-        header("Location: ../error.php?err=Could not initiate a safe session (ini_set)");
+        header("Location: ../../error.php?err=Could not initiate a safe session (ini_set)");
         exit();
     }
 
@@ -71,7 +71,7 @@ function login($email, $password, $mysqli) {
                     $now = time();
                     if (!$mysqli->query("INSERT INTO login_attempts(user_id, time) 
                                     VALUES ('$user_id', '$now')")) {
-                        header("Location: ../error.php?err=Database error: login_attempts");
+                        header("Location: ../../error.php?err=Database error: login_attempts");
                         exit();
                     }
 
@@ -84,7 +84,7 @@ function login($email, $password, $mysqli) {
         }
     } else {
         // Could not create a prepared statement
-        header("Location: ../error.php?err=Database error: cannot prepare statement");
+        header("Location: ../../error.php?err=Database error: cannot prepare statement");
         exit();
     }
 }
@@ -113,7 +113,7 @@ function checkbrute($user_id, $mysqli) {
         }
     } else {
         // Could not create a prepared statement
-        header("Location: ../error.php?err=Database error: cannot prepare statement check force");
+        header("Location: ../../error.php?err=Database error: cannot prepare statement check force");
         exit();
     }
 }
