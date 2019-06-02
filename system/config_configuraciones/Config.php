b16400d67b73ada5af77f9a3d3d16114630795e6
Erick Nunez <aerick.nunez@gmail.com>
2019-06-02 00:43:50 -0600

    Update Config.php

--- system/config_configuraciones/Config.php
+++ system/config_configuraciones/Config.php
@@ -397,7 +397,7 @@ unset($panel);
 	
 
 
-} else {
+} else { /// termina si hay iconos 
 	//save
 $return.= '<? '; 
 $return.= 'Alerts::Mensaje("No hay iconos que mostrar, por favor ingrese sus productos para poder realizar sus ventas","danger","<a href=\"?iconos\" class=\"btn btn-success\">CREAR ICONOS</a>","<a href=\"https://pizto.com/help#iconos\" class=\"btn btn-primary\" target=\"_blank\"><i class=\"fa fa-info-circle \"></i> VER COMO HACERLO </a>");'; 
