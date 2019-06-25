565724d5d9f0de3d7b385e2d4bba45ee29b38732
Erick Nunez <aerick.nunez@gmail.com>
2019-06-25 02:36:17 -0600

    Update gastos.js

--- assets/js/query/gastos.js
+++ assets/js/query/gastos.js
@@ -2,17 +2,27 @@ $(document).ready(function()
 {
 
 	$('#btn-gastos').click(function(e){ /// para el formulario
+		$('#btn-gastos').addClass('disabled');
 		e.preventDefault();
 		$.ajax({
 			url: "application/src/routes.php?op=28",
 			method: "POST",
 			data: $("#form-gastos").serialize(),
 			success: function(data){
-				$("#gastos").html(data);
 				$("#form-gastos").trigger("reset");
+				$("#gastos").html(data);			
+				setTimeout(BotonEnable, 2000); // para desactivar elboton por un rato
+		
 			}
 		})
 	})
+    
+
+    function BotonEnable(){
+        $('#btn-gastos').removeClass("disabled");
+    }
+
+
 $("#form-gastos").keypress(function(e) {//Para deshabilitar el uso de la tecla "Enter"
 if (e.which == 13) {
 return false;
@@ -29,6 +39,9 @@ return false;
 		$("#gastos").html(htmlexterno);
    	 	});
 	});
+
+
+
 
 
 });
