c9c7c0903870564eec2556b503362151749110e5
Erick Nunez <aerick.nunez@gmail.com>
2019-07-06 18:54:03 -0600

    Update root_json.php

--- application/includes/root_json.php
+++ application/includes/root_json.php
@@ -1,4 +1,6 @@
 <?
+if(is_numeric($_REQUEST["x"])){
+
 include_once '../common/Helpers.php';
 include_once 'variables_db.php';
 include_once 'db_connect.php';
@@ -8,7 +10,7 @@ include_once '../common/Mysqli.php';
 
     $db = new dbConn();
 
-    $a = $db->query("SELECT expira, expireacion, ftp_servidor, 
+    $a = $db->query("SELECT expira, expiracion, ftp_servidor, 
     	ftp_path, ftp_ruta, ftp_user, ftp_password, tipo_sistema, 
     	plataforma, pantallas, td
     	FROM config_root WHERE td = ". $_REQUEST["x"]."");
@@ -20,5 +22,5 @@ include_once '../common/Mysqli.php';
     $a->close();
 
 echo json_encode($b, false);
-
+}
 ?>
