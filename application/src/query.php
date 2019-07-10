f3f417ff4b7194d1de2719a249dd1e30c64df22a
Erick Nunez <aerick.nunez@gmail.com>
2019-07-10 16:35:31 -0600

    Update query.php

--- application/src/query.php
+++ application/src/query.php
@@ -169,15 +169,6 @@ echo '<script type="text/javascript" src="assets/js/query/sync.js?v='.$numero.'"
 elseif(isset($_GET["db_sync"])) {
 echo '<script type="text/javascript" src="assets/js/query/admin.js?v='.$numero.'"></script>';
 }
-elseif(isset($_GET["actualizacionesup"])) { // no tiene nada el script para el archivo
-echo '<script type="text/javascript" src="assets/js/query/null.js?v='.$numero.'"></script>';
-}
-elseif(isset($_GET["synclist"])) { // no tiene nada el script para el archivo
-echo '<script type="text/javascript" src="assets/js/query/null.js?v='.$numero.'"></script>';
-}
-elseif(isset($_GET["root"])) { // no tiene nada el script para el archivo
-echo '<script type="text/javascript" src="assets/js/query/null.js?v='.$numero.'"></script>';
-}
 elseif(isset($_GET["edocortes"])) {
 echo '<script type="text/javascript" src="assets/js/query/admin.js?v='.$numero.'"></script>';
 }
