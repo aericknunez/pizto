60a17331fd7bc377cca5b4c3b4946d9c93a41596
Erick Nunez <aerick.nunez@gmail.com>
2019-07-09 23:24:22 -0600

    Create 0-0-999997ac0c50bb333335cc85e9411111.sql

---
+++ sync/sql/0-0-999997ac0c50bb333335cc85e9411111.sql
@@ -0,0 +1,14 @@
+TRUNCATE TABLE `ticket_num`;
+
+TRUNCATE TABLE `ticket`;
+
+TRUNCATE TABLE `mesa_nombre`;
+
+TRUNCATE TABLE `mesa`;
+
+TRUNCATE TABLE `gastos`;
+
+TRUNCATE TABLE `corte_diario`;
+
+TRUNCATE TABLE `corte_diario`;
+
