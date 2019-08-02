898bc3904dbc141609c1f55f36178f7bffdc9902
Erick Nunez <aerick.nunez@gmail.com>
2019-08-02 13:48:06 -0600

    regreso

--- index.php
+++ index.php
@@ -6,30 +6,29 @@ include_once 'application/includes/functions.php';
 sec_session_start();
 include_once 'application/includes/register.inc.php'; // [para registrar]
 
-header("location: index.html")
 
-// if (login_check($mysqli) == true) {
-//     include_once 'catalog/index.php';
-// } else {
+if (login_check($mysqli) == true) {
+    include_once 'catalog/index.php';
+} else {
 	
-// 	if(isset($_REQUEST["change"])){
-// 		if($_SESSION["inicio"] == NULL){
-// 			$_SESSION["inicio"] = 1;
-// 		} else {
-// 			unset($_SESSION["inicio"]);
-// 		}
-// 	 header("location: ./");
-// 	}
-//     //include_once 'catalog/login.php';
-//     	if(Helpers::ServerDomain() == FALSE){
-// 		  		if($_SESSION["inicio"] == NULL){
-// 					include_once 'catalog/login_user.php';
-// 				} else {
-// 					include_once 'catalog/login.php';
-// 				}
-// 		} else {
-// 			include_once 'catalog/login.php';
-// 		}   
+	if(isset($_REQUEST["change"])){
+		if($_SESSION["inicio"] == NULL){
+			$_SESSION["inicio"] = 1;
+		} else {
+			unset($_SESSION["inicio"]);
+		}
+	 header("location: ./");
+	}
+    //include_once 'catalog/login.php';
+    	if(Helpers::ServerDomain() == FALSE){
+		  		if($_SESSION["inicio"] == NULL){
+					include_once 'catalog/login_user.php';
+				} else {
+					include_once 'catalog/login.php';
+				}
+		} else {
+			include_once 'catalog/login.php';
+		}   
 
-// }
+}
 ?>
