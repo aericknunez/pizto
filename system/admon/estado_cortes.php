757ab6ae29902f0a45118b648443c29d0a345fb3
Erick Nunez <aerick.nunez@gmail.com>
2019-08-05 16:24:24 -0600

    Update estado_cortes.php

--- system/admon/estado_cortes.php
+++ system/admon/estado_cortes.php
@@ -1,6 +1,11 @@
 <?php 
 defined('BASEPATH') OR exit('No direct script access allowed');
+include_once 'application/common/Mysqli.php';
+$db = new dbConn();
 
+
+include_once 'system/admon/Admin.php';
+$admon = new Admin;
 
 ?>
   <div class="row justify-content-md-center">
