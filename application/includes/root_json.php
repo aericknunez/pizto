aa61de3a119a8dd5e192c414d5dc74b5ca73327b
Erick Nunez <aerick.nunez@gmail.com>
2019-08-27 22:09:30 -0600

    json

--- application/includes/root_json.php
+++ application/includes/root_json.php
@@ -3,13 +3,15 @@ if(is_numeric($_REQUEST["x"])){
 
 include_once '../common/Helpers.php';
 include_once 'variables_db.php';
-sec_session_start();
 include_once '../common/Mysqli.php';
 $db = new dbConn();
 include_once 'DataLogin.php';
 
 $seslog = new Login();
 $seslog->sec_session_start();
+
+
+
     $a = $db->query("SELECT expira, expiracion, ftp_servidor, 
     	ftp_path, ftp_ruta, ftp_user, ftp_password, tipo_sistema, 
     	plataforma, pantallas, td
--- sync/get_json.php
+++ sync/get_json.php
@@ -1,7 +1,6 @@
 <?php
 include_once '../application/common/Helpers.php'; // [Para todo]
 include_once '../application/includes/variables_db.php';
-include_once '../application/includes/db_connect.php';
 include_once '../application/common/Mysqli.php';
 include_once '../application/includes/DataLogin.php';
 $db = new dbConn();
@@ -19,75 +18,75 @@ $hora = date("H:i:s");
 
 
 
-$data =  file_get_contents('https://pizto.com/admin/application/includes/db_sync_json.php?x=' . $_SESSION["temporal_td"]); 
-$datos = json_decode($data, true);
+// $data =  file_get_contents('https://pizto.com/admin/application/includes/db_sync_json.php?x=' . $_SESSION["temporal_td"]); 
+// $datos = json_decode($data, true);
 
-foreach ($datos as $valores) { // vamos hacer un archivo por cada hash
-	$sync = $valores["hash"];
+// foreach ($datos as $valores) { // vamos hacer un archivo por cada hash
+// 	$sync = $valores["hash"];
 
-$a = $db->query("SELECT * FROM login_db_sync WHERE hash = '$sync'");
-if($a->num_rows == 0){
+// $a = $db->query("SELECT * FROM login_db_sync WHERE hash = '$sync'");
+// if($a->num_rows == 0){
 
-	$handle = fopen($sync . ".sql",'w+');
-	$resultado.= 'INSERT INTO login_sync VALUES("", "'.$sync.'", "4", "1",  "'.$fecha.'", "'.$hora.'", "'.$_SESSION["temporal_td"].'");';
+// 	$handle = fopen($sync . ".sql",'w+');
+// 	$resultado.= 'INSERT INTO login_sync VALUES("", "'.$sync.'", "4", "1",  "'.$fecha.'", "'.$hora.'", "'.$_SESSION["temporal_td"].'");';
 	
-	fwrite($handle,$resultado);
-	unset($resultado);
-	fclose($handle);
+// 	fwrite($handle,$resultado);
+// 	unset($resultado);
+// 	fclose($handle);
 
-	$dato = array();
-	$dato["hash"] = $sync;
-	$dato["fecha"] = $fecha;
-	$dato["hora"] = $hora;
-	$datos["hash"] = Helpers::HashId();
-	$datos["time"] = Helpers::TimeId();
-	$db->insert("login_db_sync", $dato);
+// 	$dato = array();
+// 	$dato["hash"] = $sync;
+// 	$dato["fecha"] = $fecha;
+// 	$dato["hora"] = $hora;
+// 	$datos["hash"] = Helpers::HashId();
+// 	$datos["time"] = Helpers::TimeId();
+// 	$db->insert("login_db_sync", $dato);
 
-// aqui ejecuto el sql que deberia estar en el directorio descargado de git
-$archx = "sql/" . $sync . ".sql";            
+// // aqui ejecuto el sql que deberia estar en el directorio descargado de git
+// $archx = "sql/" . $sync . ".sql";            
 
-if (file_exists($archx)) {
-$sql = explode(";",file_get_contents($archx));//
-foreach($sql as $query){
-@$db->query($query);
-} @unlink($archx); } 
+// if (file_exists($archx)) {
+// $sql = explode(";",file_get_contents($archx));//
+// foreach($sql as $query){
+// @$db->query($query);
+// } @unlink($archx); } 
 
-//
-	} $a->close();
-
-
-	if($sync != NULL){
-		if(SubirFtp($sync) == TRUE){
-			@unlink($sync . ".sql");	
-		}	 
-	} 
+// //
+// 	} $a->close();
 
 
-} // foreach
+// 	if($sync != NULL){
+// 		if(SubirFtp($sync) == TRUE){
+// 			@unlink($sync . ".sql");	
+// 		}	 
+// 	} 
+
+
+// } // foreach
 
 
 
 
-function SubirFtp($sync){
-	include_once '../system/sync/Ftp.php';
-		$subir =  new Ftp;
-		if($subir->Servidor("ftp.pizto.com",
-						"erick@pizto.com",
-						"caca007125-",
-						$sync . ".sql",
-						"/admin/sync/db/",
-						"C:/AppServ/www/pizto/sync/". $sync .".sql") == TRUE){
-						return TRUE;
-		} else {
-			return FALSE;
-		}
-}
+// function SubirFtp($sync){
+// 	include_once '../system/sync/Ftp.php';
+// 		$subir =  new Ftp;
+// 		if($subir->Servidor("ftp.pizto.com",
+// 						"erick@pizto.com",
+// 						"caca007125-",
+// 						$sync . ".sql",
+// 						"/admin/sync/db/",
+// 						"C:/AppServ/www/pizto/sync/". $sync .".sql") == TRUE){
+// 						return TRUE;
+// 		} else {
+// 			return FALSE;
+// 		}
+// }
 
 
 
 ///////// actualizar el root
 $data =  file_get_contents('https://pizto.com/admin/application/includes/root_json.php?x=' . $_SESSION["temporal_td"]); 
 $cambio = json_decode($data, true);
-Helpers::UpdateId("config_root", $cambio, "td=" . $_SESSION["temporal_td"]);
+// Helpers::UpdateId("config_root", $cambio, "td=" . $_SESSION["temporal_td"]);
 
 unset($_SESSION["temporal_td"]);
