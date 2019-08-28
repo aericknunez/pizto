b25f02831e72a401a93ab222e33d225c10144b4e
Erick Nunez <aerick.nunez@gmail.com>
2019-08-27 22:10:24 -0600

    Update db_sync_json.php

--- application/includes/db_sync_json.php
+++ application/includes/db_sync_json.php
@@ -1,16 +1,12 @@
 <?
 include_once '../common/Helpers.php';
 include_once 'variables_db.php';
-sec_session_start();
 include_once '../common/Mysqli.php';
 $db = new dbConn();
 include_once 'DataLogin.php';
 
 $seslog = new Login();
 $seslog->sec_session_start();
-
-
-    $db = new dbConn();
 
      $a = $db->query("SELECT hash FROM login_db_sync");
 		$data = array();
