ac4f80de3053235d06249fa160b05f54b8493c6b
Erick Nunez <aerick.nunez@gmail.com>
2020-03-19 23:56:12 -0600

    Update head.php

--- catalog/body/head.php
+++ catalog/body/head.php
@@ -3,7 +3,7 @@
   // header("Expires: Sat, 1 Jul 2000 05:00:00 GMT"); // Fecha en el pasado
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
